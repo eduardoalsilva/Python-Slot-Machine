2db7c23498cade8dde91398e68202b73012bce79
Eduardo Silva <eduardoalsilva45@gmail.com>
2022-12-03 18:08:34 -0300

    Corrigindo erro no arquivo main.py

--- main.py
+++ main.py
@@ -66,7 +66,7 @@ def print_slot_machine(columns):
 
 def deposit():
     while True:
-        amount = input("What would you like to deposit? ")
+        amount = input("What would you like to deposit? $")
         if amount.isdigit():
             amount = int(amount)
             if amount > 0:
@@ -133,5 +133,5 @@ def main():
             break
         balance += spin(balance)
 
-
+    print(f"You left with ${balance}")
 main()
